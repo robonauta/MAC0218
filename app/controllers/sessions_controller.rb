b94958848737d30c4f11a4c72671cea51b1ca302
batmacumba <brunocarneirodacunha@usp.br>
2018-05-06 16:46:50 -0300

    login acha o email, falta o password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    p params[:user :email]
+    user = User.find_by(email: params[:user][:email])
     # If the user exists AND the password entered is correct.
-#    if (@user && @user.authenticate(params[:password]))
-    if (user)
+    if (user && user.authenticate(params[:user][:password]))
+#    if (user)
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
-      p "achei o usuário"
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
-      p "nao achei"
       redirect_to '/login'
     end
   end
