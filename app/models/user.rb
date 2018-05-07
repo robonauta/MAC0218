bc1ec70ee5619adfe30133b3f7740206a37f4fb9
batmacumba <brunocarneirodacunha@usp.br>
2018-05-07 02:21:07 -0300

    signup funcionando

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
 #  attr_accessor :username, :email, :password, :password_confirmation
-  has_secure_password
   attr_accessor :password, :password_confirmation
   EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
