e136d131a54aae8fdf6f44a863bbef6ccfc38573
robonauta <pedro.hba@hotmail.com>
2018-06-10 05:06:09 -0300

    Test for login view via capybara

---
+++ spec/features/login.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+require 'spec_helper'
+
+RSpec.feature 'Visitor signs in' do
+  scenario 'with valid email and password' do
+    @user1 = create(:user, username: 'test', email: 'test@test.com', password: 'xuxuxu')
+    sign_in_with 'test@test.com', 'xuxuxu'
+    #save_and_open_page
+    expect(page).to have_content('Novo projeto')
+    DatabaseCleaner.clean
+  end
+
+  scenario 'with invalid email' do
+    sign_in_with 'invalid_email', 'password'
+    expect(page).to have_content('Entrar')
+  end
+
+  scenario 'with blank password' do
+    sign_in_with 'valid@example.com', ''
+    expect(page).to have_content('Entrar')
+  end
+
+  def sign_in_with(email, password)
+    visit '/sessions/new'
+    user = create(:user)
+    fill_in 'email', with: email, visible: false
+    fill_in 'password', with: password, visible: false
+    click_button 'Entrar'
+  end
+end
