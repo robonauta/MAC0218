a1fbfeae12fae896487714f9a1bb7ee72fdb4316
batmacumba <brunocarneirodacunha@usp.br>
2018-06-10 15:58:22 -0300

    labels#new progredindo

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -1,5 +1,16 @@
 class LabelsController < ApplicationController
+  # Vamos rotular?
   def new
+    # Pega uma imagem dentro do projeto passado como argumento  
+    if (params[:id])
+      @project = Project.find(params[:id])
+      @image = @project.images.sample
+    # Pega uma imagem aleatória
+    else 
+      count = Image.count
+      random_offset = rand(count)
+      @image = Image.offset(random_offset).first
+    end 
   end
   def create
     
