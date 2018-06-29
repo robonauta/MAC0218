0d7e070cb05a4370d35d1a663e35b33968f496e8
batmacumba <brunocarneirodacunha@usp.br>
2018-06-29 00:45:59 -0300

    fix bug user & image

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -5,14 +5,19 @@ class LabelsController < ApplicationController
     if (params[:id])
       @project = Project.find(params[:id])
       @image = @project.images.sample
+      
     # Pega uma imagem aleatória
     else 
       count = Image.count
+      # db vazio
+      redirect_to '/' and return if count == 0
       random_offset = rand(count)
       @image = Image.offset(random_offset).first
     end
-    @question = Question.where('project_id = ?', @image.project_id).sample
-    @answer_opts = AnswerOpt.where('question_id = ?', @question.id)
+    if (@image)
+      @question = Question.where('project_id = ?', @image.project_id).sample
+      @answer_opts = AnswerOpt.where('question_id = ?', @question.id)
+    end
   end
   
   def create
