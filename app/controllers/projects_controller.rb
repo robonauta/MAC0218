3a0d8edd7fab322c301438f6fe5a0543d7ee67be
robonauta <pedro.hba@hotmail.com>
2018-06-28 21:45:51 -0300

    Ressucitação da dashboard para o desenvolvedor recuperar os dados dos seus projetos

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -76,6 +76,10 @@ class ProjectsController < ApplicationController
 
     def my
       @projects = Project.where('user_id = ?', current_user.id)
-      render 'my'
+      render 'dashboard'
     end
+
+    def retrieve
+      render 'retrieve'
+    end 
 end
